ebdde26b3a325855a8ce88d0ec73cd80b80ba65d
Md Shakil Ahamed <md.shakilahamedxox72@gmail.com>
2021-09-14 13:38:33 -0400

    "future.then"

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,32 +1,37 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   Future<String> sayHelloto(String name) {
     // return Future.value("Hello $name");
     // return Future.error("error");
     return Future.delayed(Duration(seconds: 5), () {
-      return "I am data";
+      return "I am $name";
+    });
+  }
+
+  String? greetings;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    sayHelloto("trump").then((value) {
+      setState(() {
+        greetings = value;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: FutureBuilder(
-        future: sayHelloto("jahangir"),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Text(snapshot.error as String);
-          } else {
-            if (snapshot.hasData) {
-              return Text(snapshot.data as String);
-            } else {
-              return CircularProgressIndicator();
-            }
-          }
-        },
-      )),
-    );
+        body: Center(
+      child: greetings == null ? CircularProgressIndicator() : Text(greetings!),
+    ));
   }
 }
